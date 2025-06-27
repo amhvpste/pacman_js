0297c3cb995084d5d6222e3a90cd5859c733ea78
Vlad.Zabella <119269949+amhvpste@users.noreply.github.com>
2025-06-27 21:32:38 +0300

    game ready

--- pacman.js
+++ pacman.js
@@ -1,4 +1,4 @@
-// board
+// ігрове поле
 let board;
 let context;
 
@@ -8,7 +8,7 @@ const tileSize = 32;
 const boardWidth = columnCount * tileSize;
 const boardHeight = rowCount * tileSize;
 
-// images
+// зображення
 let blueGhostImage;
 let orangeGhostImage;
 let pinkGhostImage;
@@ -19,23 +19,8 @@ let pacmanLeftImage;
 let pacmanRightImage;
 let wallImage;
 
-const walls = new Set();
-const foods = new Set();
-const ghosts = new Set();
-let pacman;
-
-window.onload = function () {
-    board = document.getElementById("board");
-    board.height = boardHeight;
-    board.width = boardWidth;
-    context = board.getContext("2d");
-
-    loadImage();
-    loadMap();
-
-    update(); // ← Функція викликається!
-};
-
+// X — стіна, O — не малювати, P — Пакмен, ' ' — їжа
+// Привиди: b — синій, o — оранжевий, p — рожевий, r — червоний
 const tileMap = [
     "XXXXXXXXXXXXXXXXXXX",
     "X        X        X",
@@ -57,8 +42,60 @@ const tileMap = [
     "X    X   X   X    X",
     "X XXXXXX X XXXXXX X",
     "X                 X",
-    "XXXXXXXXXXXXXXXXXXX"
+    "XXXXXXXXXXXXXXXXXXX" 
 ];
+
+const walls = new Set();
+const foods = new Set();
+const ghosts = new Set();
+let pacman;
+
+const directions = ['U', 'D', 'L', 'R']; // Вгору, вниз, вліво, вправо
+let score = 0;
+let lives = 3;
+let gameOver = false;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); // малювання на канвасі
+
+    loadImages();
+    loadMap();
+
+    // встановити випадковий напрямок для кожного привида
+    for (let ghost of ghosts.values()) {
+        const newDirection = directions[Math.floor(Math.random()*4)];
+        ghost.updateDirection(newDirection);
+    }
+
+    update(); // запуск ігрового циклу
+    document.addEventListener("keyup", movePacman); // обробка натискань клавіш
+}
+
+function loadImages() {
+    wallImage = new Image();
+    wallImage.src = "./wall.png";
+
+    blueGhostImage = new Image();
+    blueGhostImage.src = "./blueGhost.png";
+    orangeGhostImage = new Image();
+    orangeGhostImage.src = "./orangeGhost.png"
+    pinkGhostImage = new Image()
+    pinkGhostImage.src = "./pinkGhost.png";
+    redGhostImage = new Image()
+    redGhostImage.src = "./redGhost.png";
+
+    pacmanUpImage = new Image();
+    pacmanUpImage.src = "./pacmanUp.png";
+    pacmanDownImage = new Image();
+    pacmanDownImage.src = "./pacmanDown.png";
+    pacmanLeftImage = new Image();
+    pacmanLeftImage.src = "./pacmanLeft.png";
+    pacmanRightImage = new Image();
+    pacmanRightImage.src = "./pacmanRight.png";
+}
 
 function loadMap() {
     walls.clear();
@@ -70,27 +107,33 @@ function loadMap() {
             const row = tileMap[r];
             const tileMapChar = row[c];
 
-            const x = c * tileSize;
-            const y = r * tileSize;
+            const x = c*tileSize;
+            const y = r*tileSize;
 
-            if (tileMapChar == 'X') {
+            if (tileMapChar == 'X') { // блок стіни
                 const wall = new Block(wallImage, x, y, tileSize, tileSize);
-                walls.add(wall); // ← додано в walls
-            } else if (tileMapChar == 'b') {
+                walls.add(wall);  
+            }
+            else if (tileMapChar == 'b') { // синій привид
                 const ghost = new Block(blueGhostImage, x, y, tileSize, tileSize);
                 ghosts.add(ghost);
-            } else if (tileMapChar == 'o') {
+            }
+            else if (tileMapChar == 'o') { // оранжевий привид
                 const ghost = new Block(orangeGhostImage, x, y, tileSize, tileSize);
                 ghosts.add(ghost);
-            } else if (tileMapChar == 'p') {
+            }
+            else if (tileMapChar == 'p') { // рожевий привид
                 const ghost = new Block(pinkGhostImage, x, y, tileSize, tileSize);
                 ghosts.add(ghost);
-            } else if (tileMapChar == 'r') {
+            }
+            else if (tileMapChar == 'r') { // червоний привид
                 const ghost = new Block(redGhostImage, x, y, tileSize, tileSize);
                 ghosts.add(ghost);
-            } else if (tileMapChar == 'P') {
+            }
+            else if (tileMapChar == 'P') { // Пакмен
                 pacman = new Block(pacmanRightImage, x, y, tileSize, tileSize);
-            } else if (tileMapChar == ' ') {
+            }
+            else if (tileMapChar == ' ') { // їжа
                 const food = new Block(null, x + 14, y + 14, 4, 4);
                 foods.add(food);
             }
@@ -99,32 +142,152 @@ function loadMap() {
 }
 
 function update() {
+    if (gameOver) return;
+
+    move();
     draw();
-    setTimeout(update, 50);
+    setTimeout(update, 50); // приблизно 20 кадрів на секунду
 }
 
 function draw() {
-    // Очистка
-    context.clearRect(0, 0, board.width, board.height);
+    context.clearRect(0, 0, board.width, board.height); // очистка полотна
 
-    // Стінки
-    for (let wall of walls) {
+    // Пакмен
+    context.drawImage(pacman.image, pacman.x, pacman.y, pacman.width, pacman.height);
+
+    // Привиди
+    for (let ghost of ghosts.values()) {
+        context.drawImage(ghost.image, ghost.x, ghost.y, ghost.width, ghost.height);
+    }
+
+    // Стіни
+    for (let wall of walls.values()) {
         context.drawImage(wall.image, wall.x, wall.y, wall.width, wall.height);
     }
 
     // Їжа
-    for (let food of foods) {
-        context.fillStyle = "white";
+    context.fillStyle = "white";
+    for (let food of foods.values()) {
         context.fillRect(food.x, food.y, food.width, food.height);
     }
 
-    // Привиди
-    for (let ghost of ghosts) {
-        context.drawImage(ghost.image, ghost.x, ghost.y, ghost.width, ghost.height);
+    // Рахунок
+    context.fillStyle = "white";
+    context.font = "14px sans-serif";
+    if (gameOver) {
+        context.fillText("Гру завершено: " + String(score), tileSize/2, tileSize/2);
+    } else {
+        context.fillText("x" + String(lives) + " " + String(score), tileSize/2, tileSize/2);
+    }
+}
+
+function move() {
+    pacman.x += pacman.velocityX;
+    pacman.y += pacman.velocityY;
+
+    // перевірка зіткнень зі стінами
+    for (let wall of walls.values()) {
+        if (collision(pacman, wall)) {
+            pacman.x -= pacman.velocityX;
+            pacman.y -= pacman.velocityY;
+            break;
+        }
     }
 
-    // Пакмен
-    context.drawImage(pacman.image, pacman.x, pacman.y, pacman.width, pacman.height);
+    // зіткнення з привидами
+    for (let ghost of ghosts.values()) {
+        if (collision(ghost, pacman)) {
+            lives--;
+            if (lives == 0) {
+                gameOver = true;
+                return;
+            }
+            resetPositions();
+        }
+
+        if (ghost.y == tileSize*9 && ghost.direction != 'U' && ghost.direction != 'D') {
+            ghost.updateDirection('U');
+        }
+
+        ghost.x += ghost.velocityX;
+        ghost.y += ghost.velocityY;
+
+        for (let wall of walls.values()) {
+            if (collision(ghost, wall) || ghost.x <= 0 || ghost.x + ghost.width >= boardWidth) {
+                ghost.x -= ghost.velocityX;
+                ghost.y -= ghost.velocityY;
+                const newDirection = directions[Math.floor(Math.random()*4)];
+                ghost.updateDirection(newDirection);
+            }
+        }
+    }
+
+    // зіткнення з їжею
+    let foodEaten = null;
+    for (let food of foods.values()) {
+        if (collision(pacman, food)) {
+            foodEaten = food;
+            score += 10;
+            break;
+        }
+    }
+    foods.delete(foodEaten);
+
+    // наступний рівень
+    if (foods.size == 0) {
+        loadMap();
+        resetPositions();
+    }
+}
+
+function movePacman(e) {
+    if (gameOver) {
+        loadMap();
+        resetPositions();
+        lives = 3;
+        score = 0;
+        gameOver = false;
+        update(); // перезапуск циклу гри
+        return;
+    }
+
+    // напрямок руху
+    if (e.code == "ArrowUp" || e.code == "KeyW") {
+        pacman.updateDirection('U');
+    }
+    else if (e.code == "ArrowDown" || e.code == "KeyS") {
+        pacman.updateDirection('D');
+    }
+    else if (e.code == "ArrowLeft" || e.code == "KeyA") {
+        pacman.updateDirection('L');
+    }
+    else if (e.code == "ArrowRight" || e.code == "KeyD") {
+        pacman.updateDirection('R');
+    }
+
+    // оновлення зображення пакмена
+    if (pacman.direction == 'U') pacman.image = pacmanUpImage;
+    else if (pacman.direction == 'D') pacman.image = pacmanDownImage;
+    else if (pacman.direction == 'L') pacman.image = pacmanLeftImage;
+    else if (pacman.direction == 'R') pacman.image = pacmanRightImage;
+}
+
+function collision(a, b) {
+    return a.x < b.x + b.width &&   // лівий край A не доходить до правого краю B
+           a.x + a.width > b.x &&   // правий край A заходить за лівий край B
+           a.y < b.y + b.height &&  // верхній край A не доходить до нижнього краю B
+           a.y + a.height > b.y;    // нижній край A заходить за верхній край B
+}
+
+function resetPositions() {
+    pacman.reset();
+    pacman.velocityX = 0;
+    pacman.velocityY = 0;
+    for (let ghost of ghosts.values()) {
+        ghost.reset();
+        const newDirection = directions[Math.floor(Math.random()*4)];
+        ghost.updateDirection(newDirection);
+    }
 }
 
 class Block {
@@ -137,34 +300,48 @@ class Block {
 
         this.startX = x;
         this.startY = y;
+
+        this.direction = 'R';
+        this.velocityX = 0;
+        this.velocityY = 0;
     }
-}
 
-function loadImage() {
-    wallImage = new Image();
-    wallImage.src = "./wall.png";
+    updateDirection(direction) {
+        const prevDirection = this.direction;
+        this.direction = direction;
+        this.updateVelocity();
+        this.x += this.velocityX;
+        this.y += this.velocityY;
 
-    blueGhostImage = new Image();
-    blueGhostImage.src = "./blueGhost.png";
+        for (let wall of walls.values()) {
+            if (collision(this, wall)) {
+                this.x -= this.velocityX;
+                this.y -= this.velocityY;
+                this.direction = prevDirection;
+                this.updateVelocity();
+                return;
+            }
+        }
+    }
 
-    orangeGhostImage = new Image();
-    orangeGhostImage.src = "./orangeGhost.png";
+    updateVelocity() {
+        if (this.direction == 'U') {
+            this.velocityX = 0;
+            this.velocityY = -tileSize / 4;
+        } else if (this.direction == 'D') {
+            this.velocityX = 0;
+            this.velocityY = tileSize / 4;
+        } else if (this.direction == 'L') {
+            this.velocityX = -tileSize / 4;
+            this.velocityY = 0;
+        } else if (this.direction == 'R') {
+            this.velocityX = tileSize / 4;
+            this.velocityY = 0;
+        }
+    }
 
-    pinkGhostImage = new Image();
-    pinkGhostImage.src = "./pinkGhost.png";
-
-    redGhostImage = new Image();
-    redGhostImage.src = "./redGhost.png";
-
-    pacmanUpImage = new Image();
-    pacmanUpImage.src = "./pacmanUp.png";
-
-    pacmanDownImage = new Image();
-    pacmanDownImage.src = "./pacmanDown.png";
-
-    pacmanLeftImage = new Image();
-    pacmanLeftImage.src = "./pacmanLeft.png";
-
-    pacmanRightImage = new Image();
-    pacmanRightImage.src = "./pacmanRight.png";
-}
+    reset() {
+        this.x = this.startX;
+        this.y = this.startY;
+    }
+};
